0f269f95cf0ec3a298372e5ca410a56a12a85005
RabishKumar <girishmahto2005@gmail.com>
2025-01-26 19:17:47 +0530

    added music and bg color interactivity and changed the border radius of profile image

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 
-let audioPlayingState = false
+let audioPlayingState = false;
+let intervalKey = 0;
+let isActive = false;
 window.addEventListener('click',(e)=>{
     let audio = document.querySelector('audio[data-key="song1"]');
     
@@ -8,8 +10,39 @@ window.addEventListener('click',(e)=>{
     if(!audioPlayingState){
         audio.play();
         audioPlayingState = true;
+        if(!isActive) {
+            intervalKey = setInterval(changeBackgroundColor,1000);
+            isActive = true;
+        }
     }else {
         audio.pause();
         audioPlayingState= false;
+        if(isActive){
+            console.log(intervalKey);
+            clearInterval(intervalKey);
+            isActive = false;
+        }
+
     }
 });
+
+const containerElement = document.querySelector('.container');
+const middleElement = document.querySelector('.middle');
+const profileImage = document.querySelector('.pf-image-in');
+
+let currColor = 'hsl(47, 0%, 63%)';
+containerElement.style.backgroundColor = currColor;
+middleElement.style.backgroundColor = currColor;
+profileImage.style.borderColor = currColor;
+let percentage = 0;
+
+function changeBackgroundColor(){
+    containerElement.style.backgroundColor = currColor;
+    middleElement.style.backgroundColor = currColor;
+    profileImage.style.borderColor = currColor;
+    if(percentage >= 100) percentage = 0;
+    percentage++;
+    currColor = `hsl(47, ${percentage}%, 63%)`;
+}
+
+
